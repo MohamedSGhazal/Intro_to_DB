78f8a7a2387feb0daec0bfabd9da501f5e486540
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-07-29 00:06:52 +0300

    .

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 -- Use the database
 USE alx_book_store;
--- Create the A table
-CREATE TABLE A (
+-- Create the Authors table
+CREATE TABLE Authors (
   author_id INT AUTO_INCREMENT PRIMARY KEY,
   author_name VARCHAR(215) NOT NULL
 );
@@ -14,7 +14,7 @@ CREATE TABLE Books (
   author_id INT,
   price DOUBLE NOT NULL,
   publication_date DATE,
-  FOREIGN KEY (author_id) REFERENCES A(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 -- Create the Customers table
 CREATE TABLE Customers (
